a38234bf6223e9b3f18076ac8a05aca781a5ec0f
unknown <jaishish.ranjan01@gmail.com>
2025-10-26 14:23:51 +0530

    compact class and instance main method

---
+++ features/CompactMain.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+
+class CompactMain {
+    //Old Style of writing main
+//    public static void main(String[] args) {
+//        System.out.println("Old Implementation.");
+//    }
+
+    /**
+     * No static modifier is required as
+     */
+    void main() {
+        System.out.println("New Style Of Writing.");
+
+        //another way of
+        IO.println("Another way to print by : IO. println");
+
+        //
+
+    }
+}
---
+++ features/WithoutMain.java
@@ -0,0 +1,7 @@
+/**
+ * No Need to define the class if you are going
+ */
+
+void main() {
+    IO.println("Without Main");
+}
